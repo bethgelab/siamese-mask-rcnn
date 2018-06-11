1515613cfdd80d590e88a45a873b1795c6b4774c
Ivan Ustyuzhaninov <ivanustyuzh@gmail.com>
2018-06-11 15:57:43 +0200

    add a tool to load ade20k masks

---
+++ Mask_RCNN/samples/ade20k/loadAde20K.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def loadAde20K(filename):
+    """
+    Python translation of the original MATLAB function.
+    So far it only loads the semantic and instance segemntation masks.
+
+    The outputs are
+        - Object class masks (n x m)
+        - Object instance masks (n x m)
+    """
+    fileseg = filename.replace('.jpg', '_seg.png')
+
+    seg = plt.imread(fileseg)
+
+    R, G, B = (seg[:,:,0], seg[:,:,1], seg[:,:,2])
+
+    ObjectClassMasks = (R * 255 * 256 / 10 + G * 255).astype(np.int)
+
+    _, Minstances_hat = np.unique(B.ravel(), return_inverse=True)
+    ObjectInstanceMasks = np.reshape(Minstances_hat, B.shape).astype(np.int)
+
+    return ObjectClassMasks, ObjectInstanceMasks
+
+# loadAde20K('ADE20K_2016_07_26/images/training/a/airport_terminal/ADE_train_00000001.jpg')
+

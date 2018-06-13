a7afb31078bf07d9d589beb1e864215723ec8425
Ivan Ustyuzhaninov <ivanustyuzh@gmail.com>
2018-06-13 21:07:12 +0200

    add parts masks to ade20k

--- Mask_RCNN/samples/ade20k/ade20k.py
+++ Mask_RCNN/samples/ade20k/ade20k.py
@@ -17,8 +17,7 @@ from mrcnn import utils
 class ADE20KConfig(Config):
     NAME = 'ADE20K'
     IMAGES_PER_GPU = 2
-    # GPU_count = 8
-    NUM_CLASSES = 1 + 3147
+    NUM_CLASSES = 1 + 2693
 
 class ADE20KDataset(utils.Dataset):
     def load_ade20k(self, dataset_dir, subset, class_ids=None, class_map=None):
@@ -27,8 +26,8 @@ class ADE20KDataset(utils.Dataset):
         filenames_relative = [folder[0] + '/' + filename[0] for folder, filename in zip(index[1][0], index[0][0])]
         filenames          = [dataset_dir + '/' + '/'.join(f.split('/')[1:]) for f in filenames_relative]
         objectnames        = ['dummy_background'] + [f[0] for f in index[6][0]]
-        # objectPresence     = index[4].astype(np.int) - index[3].astype(np.int) # subtract the object parts
-        objectPresence     = (index[4] > 0) & (index[3] == 0) # subtract the object parts
+        objectPresence     = index[4] > 0
+        # objectPresence     = (index[4] > 0) & (index[3] == 0) # subtract the object parts
 
         with open(dataset_dir + '/image_sizes.pkl', 'rb') as f:
             image_sizes = pickle.load(f)
@@ -83,7 +82,7 @@ class ADE20KDataset(utils.Dataset):
 
         # Build mask of shape [height, width, instance_count] and list
         # of class IDs that correspond to each channel of the mask.
-        object_mask, instance_mask = loadADE20K(self.image_info[image_id]['path'])
+        object_mask, instance_mask, parts_object_mask, parts_instance_mask = loadADE20K(self.image_info[image_id]['path'])
 
         for instance_id in np.unique(instance_mask):
             instance_id_mask = (instance_mask == instance_id)
@@ -91,6 +90,16 @@ class ADE20KDataset(utils.Dataset):
                 continue
             instance_masks.append(instance_id_mask)
             class_ids.append(int(np.median(object_mask[instance_id_mask])))
+
+        if len(parts_object_mask) > 0:
+            _, _, n_levels = parts_obejects_mask.shape
+            for level in range(n_levels):
+                for instance_id in np.unique(parts_instance_mask[:,:,level]):
+                    instance_id_mask = (parts_instance_mask[:,:,level] == instance_id)
+                    if np.sum(instance_id_mask) < 1:
+                        continue
+                    instance_masks.append(instance_id_mask)
+                    class_ids.append(int(np.median(parts_object_mask[:,:,level][instance_id_mask])))
 
         if class_ids:
             mask = np.stack(instance_masks, axis=2).astype(np.bool)
@@ -100,15 +109,15 @@ class ADE20KDataset(utils.Dataset):
             # Call super class to return an empty mask
             return super(ADE20KDataset, self).load_mask(image_id)
 
-def loadADE20K(filename):
+def loadAde20K(filename):
     """
     Python translation of the original MATLAB function.
-    So far it only loads the semantic and instance segemntation masks.
 
     The outputs are
         - Object class masks (n x m)
         - Object instance masks (n x m)
     """
+    # Full obeject masks
     fileseg = filename.replace('.jpg', '_seg.png')
 
     seg = plt.imread(fileseg)
@@ -120,5 +129,23 @@ def loadADE20K(filename):
     _, Minstances_hat = np.unique(B.ravel(), return_inverse=True)
     ObjectInstanceMasks = np.reshape(Minstances_hat, B.shape).astype(np.int)
 
-    return ObjectClassMasks, ObjectInstanceMasks
+    # Parts masks
+    level = 0
+    PartsClassMasks = [];
+    PartsInstanceMasks = [];
+    while True:
+        level = level + 1
+        file_parts = filename.replace('.jpg', '_parts_{}.png'.format(level))
+        if os.path.isfile(file_parts):
+            partsseg = plt.imread(file_parts);
+            R, G, B = (partsseg[:,:,0], partsseg[:,:,1], partsseg[:,:,2])
+            PartsClassMasks.append((R * 255 * 256 / 10 + G * 255).astype(np.int))
+            _, Minstances_hat = np.unique(B.ravel(), return_inverse=True)
+            PartsInstanceMasks.append(np.reshape(Minstances_hat, B.shape).astype(np.int))
+        else:
+            if len(PartsClassMasks) > 0:
+                PartsClassMasks = np.stack(PartsClassMasks, -1)
+                PartsInstanceMasks = np.stack(PartsInstanceMasks, -1)
+            break
 
+    return ObjectClassMasks, ObjectInstanceMasks, PartsClassMasks, PartsInstanceMasks
--- Mask_RCNN/samples/ade20k/loadAde20K.py
+++ Mask_RCNN/samples/ade20k/loadAde20K.py
@@ -1,15 +1,16 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def loadADE20K(filename):
+def loadAde20K(filename):
     """
     Python translation of the original MATLAB function.
-    So far it only loads the semantic and instance segemntation masks.
 
     The outputs are
         - Object class masks (n x m)
         - Object instance masks (n x m)
     """
+    # Full obeject masks
     fileseg = filename.replace('.jpg', '_seg.png')
 
     seg = plt.imread(fileseg)
@@ -21,7 +22,26 @@ def loadADE20K(filename):
     _, Minstances_hat = np.unique(B.ravel(), return_inverse=True)
     ObjectInstanceMasks = np.reshape(Minstances_hat, B.shape).astype(np.int)
 
-    return ObjectClassMasks, ObjectInstanceMasks
+    # Parts masks
+    level = 0
+    PartsClassMasks = [];
+    PartsInstanceMasks = [];
+    while True:
+        level = level + 1
+        file_parts = filename.replace('.jpg', '_parts_{}.png'.format(level))
+        if os.path.isfile(file_parts):
+            partsseg = plt.imread(file_parts);
+            R, G, B = (partsseg[:,:,0], partsseg[:,:,1], partsseg[:,:,2])
+            PartsClassMasks.append((R * 255 * 256 / 10 + G * 255).astype(np.int))
+            _, Minstances_hat = np.unique(B.ravel(), return_inverse=True)
+            PartsInstanceMasks.append(np.reshape(Minstances_hat, B.shape).astype(np.int))
+        else:
+            if len(PartsClassMasks) > 0:
+                PartsClassMasks = np.stack(PartsClassMasks, -1)
+                PartsInstanceMasks = np.stack(PartsInstanceMasks, -1)
+            break
 
-# loadAde20K('ADE20K_2016_07_26/images/training/a/airport_terminal/ADE_train_00000001.jpg')
+    return ObjectClassMasks, ObjectInstanceMasks, PartsClassMasks, PartsInstanceMasks
+
+# loadAde20K('ADE20K_2016_07_26/images/training/b/bathroom/ADE_train_00000100.jpg')
 

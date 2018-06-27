6e1c993b1a9c118792ca1e353cfab06c1acc8e8d
Ivan Ustyuzhaninov <ivanustyuzh@gmail.com>
2018-06-27 15:21:36 +0200

    add a function for plotting the teaser figure

--- utils.py
+++ utils.py
@@ -431,7 +431,7 @@ def display_siamese_instances(target, image, boxes, masks, class_ids,
     target_ax.set_ylim(target_height + 10, -10)
     target_ax.set_xlim(-10, target_width + 10)
     target_ax.axis('off')
-    target_ax.set_title('target')
+    # target_ax.set_title('target')
 
     masked_image = image.astype(np.uint32).copy()
     for i in range(N):
@@ -607,6 +607,115 @@ def display_siamese_instances2(target_list, image_list, boxes_list, masks_list, 
             index = index + 1
 
     if auto_show:
+        plt.show()
+        
+    return
+
+def display_siamese_instances_teaser(target, image, boxes, masks, class_ids,
+                      scores=None, title="",
+                      figsize=(16, 16), ax=None,
+                      show_mask=True, show_bbox=True,
+                      colors=None, captions=None, fontsize=20,
+                      filename='teaser.pdf'):
+    """
+    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+    masks: [height, width, num_instances]
+    class_ids: [num_instances]
+    class_names: list of class names of the dataset
+    scores: (optional) confidence scores for each box
+    title: (optional) Figure title
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    captions: (optional) A list of strings to use as captions for each object
+    """
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
+    if not ax:
+        from matplotlib.gridspec import GridSpec
+        # Use GridSpec to show target smaller than image
+        fig = plt.figure(figsize=figsize)
+        gs = GridSpec(4, 5)
+        ax = plt.subplot(gs[:, 3:])
+        ax_input = plt.subplot(gs[:,:2])
+        target_ax = plt.subplot(gs[1, 2])
+        auto_show = True
+
+    # Generate random colors
+    colors = colors or visualize.random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height, 0)
+    ax.set_xlim(0, width )
+    ax.axis('off')
+    ax.set_title('Output', fontsize=fontsize)
+
+    ax_input.set_ylim(height, 0)
+    ax_input.set_xlim(0, width)
+    ax_input.axis('off')
+    ax_input.set_title('Input image', fontsize=fontsize)
+    ax_input.imshow(image)
+    
+    target_height, target_width = target.shape[:2]
+    target_ax.set_ylim(target_height, 0)
+    target_ax.set_xlim(0, target_width)
+    target_ax.axis('off')
+    target_ax.set_title('Target', fontsize=fontsize)
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = visualize.patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                alpha=0.7, linestyle="dashed",
+                                edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+
+        # Label
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            x = random.randint(x1, (x1 + x2) // 2)
+            caption = "{:.3f}".format(score) if score else 'no score'
+        else:
+            caption = captions[i]
+        ax.text(x1, y1 + 8, caption,
+                color='w', size=11, backgroundcolor="none")
+
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = visualize.apply_mask(masked_image, mask, color)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = visualize.find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = visualize.Polygon(verts, facecolor="none", edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    target_ax.imshow(target.astype(np.uint8))
+    if auto_show:
+        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
         plt.show()
         
     return
